713603a11393512e3526c3dcbcdca6af0a280c16
rLuciferMorningStar <r.lucifer2796@gmail.com>
2020-11-11 09:25:27 +0530

    Commit 2 - Working App

--- app/src/main/java/com/lessontwo/myapplication/MainActivity.kt
+++ app/src/main/java/com/lessontwo/myapplication/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.lessontwo.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.lessontwo.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Rajeshwar Singh")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+            // (it) refers to the Done button
+        }
+    }
+    private fun addNickname(view: View) {
+        binding.apply {
+            //binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        //Hiding Keyboard after typing
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/lessontwo/myapplication/MyName.kt
@@ -0,0 +1,3 @@
+package com.lessontwo.myapplication
+
+data class MyName (var name: String = "", var nickname: String = "")
